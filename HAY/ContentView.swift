fa3be74306b62d51cb132095029a134dc58d671b
jyw3927 <nagne3927@gmail.com>
2022-03-25 17:37:34 +0900

    Launch Screen updated

--- HAY/ContentView.swift
+++ HAY/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var isLoading: Bool = true
     @State private var selection = 0
     
     init() {
@@ -21,53 +22,85 @@ struct ContentView: View {
     
     var body: some View {
         
-        NavigationView {
+        ZStack {
+        
+            NavigationView {
+                
+                TabView(selection: $selection) {
+                    
+                    GameTabView(title: "게임")
+                        .tabItem {
+                            
+                            selection == 0 ? Image("game_active") : Image("game")
+                            
+                            Text("게임")
+                        }
+                        .tag(0)
+                    
+                    TestTabView(title: "심리테스트")
+                        .tabItem {
+                            
+                            selection == 1 ? Image("test_active") : Image("test")
+                            
+                            Text("심리테스트")
+                        }
+                        .tag(1)
+                    
+    //                ConversationTabView(title: "대화")
+    //                    .tabItem {
+    //
+    //                        selection == 2 ? Image("conversation_active") : Image("conversation")
+    //
+    //                        Text("대화")
+    //                    }
+    //                    .tag(2)
+    //
+    //                MypageTabView(title: "마이페이지")
+    //                    .tabItem {
+    //
+    //                        selection == 3 ? Image("mypage_active") : Image("mypage")
+    //
+    //                        Text("마이페이지")
+    //                    }
+    //                    .tag(3)
+                    
+                } // TabView
+                .accentColor(Color("EnabledColor"))
+                .navigationTitle("")
+                .navigationBarHidden(true)
+                
+            }.zIndex(0) // NavigationView
             
-            TabView(selection: $selection) {
-                
-                GameTabView(title: "게임")
-                    .tabItem {
-                        
-                        selection == 0 ? Image("game_active") : Image("game")
-                        
-                        Text("게임")
-                    }
-                    .tag(0)
-                
-                TestTabView(title: "심리테스트")
-                    .tabItem {
-                        
-                        selection == 1 ? Image("test_active") : Image("test")
-                        
-                        Text("심리테스트")
-                    }
-                    .tag(1)
-                
-                ConversationTabView(title: "대화")
-                    .tabItem {
-                        
-                        selection == 2 ? Image("conversation_active") : Image("conversation")
-                        
-                        Text("대화")
-                    }
-                    .tag(2)
-                
-                MypageTabView(title: "마이페이지")
-                    .tabItem {
-                        
-                        selection == 3 ? Image("mypage_active") : Image("mypage")
-                        
-                        Text("마이페이지")
-                    }
-                    .tag(3)
-                
-            } // TabView
-            .accentColor(Color("EnabledColor"))
-            .navigationTitle("")
-            .navigationBarHidden(true)
+            if isLoading {
+                launchScreenView.transition(.opacity).zIndex(1)
+            }
             
-        } // NavigationView
+        } // ZStack
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                withAnimation { isLoading.toggle() }
+            })
+        }
+        
     }
+}
+
+extension ContentView {
+    
+    var launchScreenView: some View {
+        
+        ZStack(alignment: .center) {
+            
+            LinearGradient(gradient: Gradient(colors: [Color("PrimaryColor"), Color("SubPrimaryColor")]),
+                            startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+            
+            Image("LaunchScreenImage")
+            
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
